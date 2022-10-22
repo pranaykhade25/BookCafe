099deeb25bb833e802586897aaa13f7edcbea507
Pranay Khade <khadepranay727@gmail.com>
2022-10-22 19:06:23 +0530

    Add files via upload

---
+++ environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+    firebaseConfig:{
+    apiKey: "AIzaSyBaLpLstn8rYGYM-cXBQKAQiyBQ7sp_3OQ",
+    authDomain: "bookstore-90f70.firebaseapp.com",
+    projectId: "bookstore-90f70",
+    storageBucket: "bookstore-90f70.appspot.com",
+    messagingSenderId: "813241746626",
+    appId: "1:813241746626:web:410dda1e9f2897ec51275b",
+    measurementId: "G-725XM5E26X"
+  
+  },
+};
